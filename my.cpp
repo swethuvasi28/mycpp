67dd12e413b624e93af997266862126a5e08bd85
swethuvasi28 <79217442+swethuvasi28@users.noreply.github.com>
2021-04-11 19:26:28 +0530

    Add files via upload

---
+++ my.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double num1,num2,product;
+	cout << "enter first number:";
+	cin >> num1;
+	cout << "enter second number:";
+	cin>> num2;
+	
+	product=num1*num2;
+	cout << "product =" << product;
+	return 0;
+}
+
